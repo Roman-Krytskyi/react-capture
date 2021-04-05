61a8d490b0de39a53ae293d5fbdcaaf152fb654f
Roman-Krytskyi <hollowdc95@gmail.com>
2021-04-05 13:40:12 +0300

    4. FAQ section added

---
+++ src/components/FaqSection.js
@@ -0,0 +1,53 @@
+const FaqSection = () => {
+  return (
+    <div>
+      <div className="faq">
+        <h2>
+          Any Questions? <span>FAQ</span>
+        </h2>
+        <div className="question">
+          <h4>How Do I Start?</h4>
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti,
+              officia.
+            </p>
+          </div>
+        </div>
+        <div className="question">
+          <h4>Daily Schedule</h4>
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti,
+              officia.
+            </p>
+          </div>
+        </div>
+        <div className="question">
+          <h4>Payment Methods</h4>
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti,
+              officia.
+            </p>
+          </div>
+        </div>
+        <div className="question">
+          <h4>What Products Do You Offer?</h4>
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti,
+              officia.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
