1d5cacfe6444ec2d623d18a64718d95c9e248510
Roman-Krytskyi <hollowdc95@gmail.com>
2021-04-05 13:03:48 +0300

    2. About first section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+//importing components
+import AboutUs from './pages/AboutUs';
+
 function App() {
   return (
     <div className="App">
-      <h1>Film</h1>
+      <AboutUs />
     </div>
   );
 }
