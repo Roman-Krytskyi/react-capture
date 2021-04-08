742d14680a4331c2b9b6234532736a0357d3dae8
Roman-Krytskyi <hollowdc95@gmail.com>
2021-04-08 13:32:05 +0300

    6. Styles have been added to Services section

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,6 +1,7 @@
 import home1 from '../img/home1.png';
-//Styleed
+//Styled
 import styled from 'styled-components';
+import { About, Description, Img, Hide } from '../components/styles';
 
 const AboutSection = () => {
   return (
@@ -31,31 +32,5 @@ const AboutSection = () => {
     </About>
   );
 };
-
-//Styled Component
-const About = styled.div`
-  min-height: 90vh;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 5rem 10rem;
-  color: white;
-`;
-const Description = styled.div`
-  flex: 1;
-  padding-right: 10rem;
-  h2 {
-    font-weight: lighter;
-  }
-`;
-
-const Img = styled.div`
-  flex: 1;
-  overflow: hidden;
-`;
-
-const Hide = styled.div`
-  overflow: hidden;
-`;
 
 export default AboutSection;
