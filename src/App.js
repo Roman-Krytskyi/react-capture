ab025fa2e8d863df03ef3e5a1cee199544aab541
Roman-Krytskyi <hollowdc95@gmail.com>
2021-04-08 17:05:07 +0300

    7. Router features have been applied

--- src/App.js
+++ src/App.js
@@ -2,14 +2,28 @@
 import GlobalStyle from './components/GlobalStyle';
 //Importing Components
 import AboutUs from './pages/AboutUs';
+import OurWork from './pages/OurWork';
+import ContactUs from './pages/ContactUs';
 import Nav from './components/Nav';
+//Router
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contacts">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
