7f62859ab6f2288c4db740de92ac039ad49f818c
Roman-Krytskyi <hollowdc95@gmail.com>
2021-04-19 11:43:28 +0300

    Framer Motion animation have been applied

--- src/styles.js
+++ src/styles.js
@@ -11,6 +11,7 @@ export const About = styled.div`
 export const Description = styled.div`
   flex: 1;
   padding-right: 10rem;
+  z-index: 2;
   h2 {
     font-weight: lighter;
   }
@@ -19,6 +20,7 @@ export const Description = styled.div`
 export const Img = styled.div`
   flex: 1;
   overflow: hidden;
+  z-index: 2;
   img {
     width: 100%;
     height: 90vh;
